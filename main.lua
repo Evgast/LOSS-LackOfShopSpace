f75079710c1809a5757eae5d1d2c22cfb39b1e3d
Evgast <136436468+Evgast@users.noreply.github.com>
2026-03-31 04:52:15 +1100

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,6 +7,16 @@ SMODS.current_mod.description_loc_vars = function()
     return {background_colour = G.C.CLEAR, text_colour = G.C.WHITE, scale = 1.2, shadow = true}
 end
 
+function LOSS.merge_tables(table_o, table_n)
+    for i = 1, #table_o do
+        if type(table_o[i]) == "function" then
+            table_n[i] = table_o[i]()
+        else
+            table_n[i] = table_o[i]
+        end
+    end
+end
+
 local wheelmove_hook = love.wheelmoved or function() end
 function love.wheelmoved(x, y)
     G.wheel_scroll = y
