78349ed6acc650bbed38b704d37bf588e7b406d2
Evgast <136436468+Evgast@users.noreply.github.com>
2026-04-02 02:53:04 +1100

    zeebee scrollbar

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ G.high_buttons = {}
 
 LOSS = SMODS.current_mod
 
-SMODS.current_mod.description_loc_vars = function()
+LOSS.description_loc_vars = function()
     return {background_colour = G.C.CLEAR, text_colour = G.C.WHITE, scale = 1.2, shadow = true}
 end
 
@@ -18,20 +18,3 @@ function LOSS.merge_tables(table_o, table_n)
         end
     end
 end
-
-local wheelmove_hook = love.wheelmoved or function() end
-function love.wheelmoved(x, y)
-    G.wheel_scroll = y
-	wheelmove_hook(x, y)
-    G.E_MANAGER:add_event(Event({
-        trigger = "after", 
-        delay = 0.1,
-        blocking = false,
-        blockable = false,
-        func = function() 
-            G.wheel_scroll = 0
-            return true 
-        end
-    }))
-end
-
